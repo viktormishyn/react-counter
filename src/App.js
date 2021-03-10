4a4defb0aa4e0e02a2aaa0f2f3c7e87c0011713c
Viktor Mishyn <viktormishyn@gmail.com>
2021-03-10 13:31:17 +0200

    Create a hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-
+import {useState} from 'react'
 
 function App() {
+    // runs every time we render the component, slowing down the performance
+    // const [count, setCount] = useState(0)
+
+    // also if we pass a function it will run every time we render the component
+    // const initialCount() {
+    //   console.log('run function')
+    //   return 0
+    // }
+    // const [count, setCount] = useState(initialCount)
+
+    // runs only the first time the component renders
+    const [count, setCount] = useState(() => {
+        console.log('run function')
+        return 0
+    })
+
+    const decrementCount = () => {
+        if (count > 0) {
+            setCount(count-1)
+        }
+        // setCount(prevCount => prevCount - 1)
+        // setCount(prevCount => prevCount - 1)
+    }
+
+    const incrementCount = () => {
+        setCount(count + 1)
+    }
+
     return (
         <>
-            <button>-</button>
-            <span>0</span>
-            <button>+</button>
+            <button onClick={decrementCount}>-</button>
+            <span>{count}</span>
+            <button onClick={incrementCount}>+</button>
         </>
     );
 }
