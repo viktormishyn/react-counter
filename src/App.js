592c7e009e0df0408980cdbabc2bd95256592c22
Viktor Mishyn <viktormishyn@gmail.com>
2021-06-07 12:23:01 +0300

    ============= Learn useReducer hook =============

--- src/App.js
+++ src/App.js
@@ -1,50 +1,30 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-function App() {
-    const [state, setState] = useState({count: 0, theme: "blue"})
-    const count = state.count
-    const theme = state.theme
-    // // or we can create 2 different hooks instead:
-    // const [count, setCount] = useState(0)
-    // const [theme, setTheme] = useState('blue')
-    // const decrementCount = () => {
-    //     if (count > 0) {
-    //         setCount(prevCount => prevCount - 1)
-    //     }
-    // }
+export default function App() {
+  const [state, setState] = useState({ count: 0, theme: "blue" });
+  const count = state.count;
 
-    const decrementCount = () => {
-        if (count > 0) {
-            setState(prevState => {
-                return {...prevState, count: prevState.count - 1}
-            })
-        }
+  function decrement() {
+    if (count > 0) {
+      setState((prevState) => {
+        return { ...prevState, count: prevState.count - 1 };
+      });
     }
+  }
 
-    const incrementCount = () => {
-        setState(prevState => {
-            return {...prevState, count: prevState.count + 1}
-        })
-    }
+  function increment() {
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count + 1 };
+    });
+  }
 
-    const changeTheme = () => {
-        setState(prevState => {
-            return prevState.theme === 'blue' ? {...prevState, theme: 'red'} : {...prevState, theme: 'blue'}
-        })
-    }
-
-    return (
-        <>
-            <div>
-                <button onClick={decrementCount}>-</button>
-                <span>{count}</span>
-                <button onClick={incrementCount}>+</button>
-            </div>
-            <div>
-                <button onClick={changeTheme}>{theme}</button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
 }
-
-export default App;
